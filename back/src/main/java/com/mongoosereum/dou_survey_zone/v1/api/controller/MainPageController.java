8a329a2760bfc1287edc6a7cf963c9c91f86610c
hwani <ojh2134@gmail.com>
2021-11-20 01:12:35 +0900

    Feat_Back/MySurvey

--- back/src/main/java/com/mongoosereum/dou_survey_zone/v1/api/controller/MainPageController.java
+++ back/src/main/java/com/mongoosereum/dou_survey_zone/v1/api/controller/MainPageController.java
@@ -11,14 +11,14 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @Slf4j
-@RequestMapping(path = "/api/v1/main", produces = MediaType.APPLICATION_JSON_VALUE)
+@RequestMapping(path = "/api/v1", produces = MediaType.APPLICATION_JSON_VALUE)
 public class MainPageController {
 
     @Autowired
     private PartService partService;
 
     /*누적이용자수*/
-    @GetMapping(path="/acc")
+    @GetMapping(path="/main/acc")
     public ResponseEntity<?> accUser() {
         Part_Acc_MySQL result = partService.partACC();
         return ResponseEntity.ok().body(result);
