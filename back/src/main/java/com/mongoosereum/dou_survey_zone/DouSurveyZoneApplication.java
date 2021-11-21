99236b3a14ed7d145f25e36f3a2d827cbcb42246
hwani <ojh2134@gmail.com>
2021-11-21 15:53:13 +0900

    Refactor_Back/Enum

--- back/src/main/java/com/mongoosereum/dou_survey_zone/DouSurveyZoneApplication.java
+++ back/src/main/java/com/mongoosereum/dou_survey_zone/DouSurveyZoneApplication.java
@@ -1,12 +1,13 @@
 package com.mongoosereum.dou_survey_zone;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-//@EnableMongoRepositories
-public class DouSurveyZoneApplication {
-
+public class DouSurveyZoneApplication{
 	public static void main(String[] args) {
 		SpringApplication.run(DouSurveyZoneApplication.class, args);
 	}
