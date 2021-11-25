e5c18681857851d9bf800fb1bfaed70692fde3a5
Domo9610 <54881558+Domo9610@users.noreply.github.com>
2021-11-25 18:15:12 +0900

    Repector/userReqVer2

--- back/src/main/java/com/mongoosereum/dou_survey_zone/config/WebSecurityConfig.java
+++ back/src/main/java/com/mongoosereum/dou_survey_zone/config/WebSecurityConfig.java
@@ -36,10 +36,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeRequests() // /와 /auth/** 경로는 인증 안해도 됨
-                .antMatchers("/","/api/v1/user/**", "/api/v1/survey/**", "/api/v1/participation/**").permitAll()
+                .antMatchers("/","/api/v1/user/**", "/api/v1/survey/{}", "/api/v1/participation/**").permitAll()
                 .anyRequest()
-                .permitAll();
-                //.authenticated();
+//                .permitAll();
+                .authenticated();
 
         // fillter 등록
         // 매요청 마다
@@ -54,3 +54,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         // 모르겠으면 진우형한테 물어보기
     }
 }
+
+
+// survey/{_id} 는 GET, POST만 허용되어야 한다. 이상 전달 끝
