b4e725ac931390e1a945f524a0984bd03e6dd0b8
yu07na06 <dbsk7885@daum.net>
2021-11-25 15:32:51 +0900

    Fix_Front/CreateSurvey_delete [ modify delete error ]

--- front/src/components/survey/UI/BeforeSurvey.js
+++ front/src/components/survey/UI/BeforeSurvey.js
@@ -7,20 +7,18 @@ import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Slider from '@mui/material/Slider';
 
-const BeforeSurvey = ({marks, valuetext, Separator, addStore}) => {
+const BeforeSurvey = ({marks, addStore}) => {
     return (
         <>
-            <Separator />
-            <FormControl onChange={(e)=>addStore(e)} component="fieldset" name ="zzz">
-                <Typography style={{fontWeight:'bold'}}>성별</Typography>
+            <FormControl onChange={ (e) => addStore(e) } component="fieldset" name ="zzz">
+                <Typography style={{ fontWeight:'bold' }} sx={{ mt:2 }} >성별</Typography>
                     <RadioGroup defaultValue="M" sx={{ mx:3 }} row aria-label="gender" name="gender">
-                        <FormControlLabel value="M" control={<Radio />} label="남성" />
-                        <FormControlLabel value="W" control={<Radio  />} label="여성" />
+                        <FormControlLabel value="M" control={ <Radio /> } label="남성" />
+                        <FormControlLabel value="W" control={ <Radio  /> } label="여성" />
                     </RadioGroup>
             </FormControl>
-            <Separator />
-            <Separator />
-            <Typography style={{fontWeight:'bold'}}>연령대</Typography>
+            
+            <Typography style={{ fontWeight:'bold' }} sx={{ mt:5 }} >연령대</Typography>
             <Box sx={{ mx:4,  width: 300 }}>
                 <Slider
                     onChange={(e)=>addStore(e)}
@@ -28,7 +26,7 @@ const BeforeSurvey = ({marks, valuetext, Separator, addStore}) => {
                     track={false}
                     aria-label="Custom marks"
                     defaultValue={20}
-                    getAriaValueText={valuetext}
+                    getAriaValueText={value => `${value}`}
                     step={10}
                     valueLabelDisplay="auto"
                     marks={marks}
