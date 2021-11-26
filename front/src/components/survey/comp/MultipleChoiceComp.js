87671f543db1ea08824a73c1af375ee665ccd153
yu07na06 <dbsk7885@daum.net>
2021-11-26 14:50:51 +0900

    Feat_Front/UpdateSurvey_v1 [ADD UpdateSurvey]

--- front/src/components/survey/comp/MultipleChoiceComp.js
+++ front/src/components/survey/comp/MultipleChoiceComp.js
@@ -6,39 +6,42 @@ import Checkbox from '@mui/material/Checkbox';
 import { useDispatch } from 'react-redux';
 import { submitAction } from '../../../modules/submitReducer';
 
-const MultipleChoiceComp = ({number, setCheck, setDelIndex, ReadOnlyState, ReadOnlyData, }) => {
+const MultipleChoiceComp = ({ number, setCheck, setDelIndex, ReadOnlyState, ReadOnlyData, UpdateKey, }) => {
 
     const [select, setSelect] = useState([]); // 보기 덩어리가 들어가있음
     const [deleteIndex, setDeleteIndex] = useState(null);
     const [temp, setTemp] = useState([]);
     const [maxNum, setMaxNum] = useState();
-    const dispatch = useDispatch();
+    const [essential, setEssential] = useState(0);
     const count = useRef(-1);
-    
-    useEffect(()=>{
-        console.log("======================================");
-        console.log(ReadOnlyData);
-        
-        
-    },[])
+    const ccc = useRef(0);
+    const dispatch = useDispatch();
 
     useEffect(()=>{
-        let newAddText = null;
         if(ReadOnlyState){
-            newAddText = ReadOnlyData.selectList.map(value=>{
-                    return addText(number, value, ReadOnlyData.surQue_MaxAns)
-            })
-            setSelect([...select , newAddText]);
+            let newAddText = ReadOnlyData.selectList.map((value)=>{
+                    return addText(number, value, ReadOnlyData.surQue_MaxAns, ReadOnlyData.surQue_Essential, )
+                })
+            setSelect(newAddText);
+
+            let newAddTextId = ReadOnlyData.selectList.map((value, index)=>{
+                return `SurQue_Ans_${number}_${index}`;
+                })
+            setTemp(newAddTextId);
         }
     },[ReadOnlyState])
 
     useEffect(()=>{
-        !ReadOnlyState&&setCheck({[number]:temp});
+        if(!ReadOnlyData || UpdateKey){ //생성할때 사용하고, 수정할때 사용할꺼야
+            setCheck({[number]:temp});
+        }
     },[temp]);
 
     useEffect(()=>{
         if(deleteIndex != null){
-            let newSelect = select.filter((value)=> value.key !== deleteIndex)
+            let newSelect = select.filter((value)=> {
+                return value.key !== deleteIndex
+            })
             setSelect(newSelect);
             let newTemp = temp.filter((value)=> value !== deleteIndex)
             setTemp(newTemp);
@@ -49,54 +52,75 @@ const MultipleChoiceComp = ({number, setCheck, setDelIndex, ReadOnlyState, ReadO
         setDeleteIndex(null); // 인덱스 중복될 수 있으니
     },[select])
 
-        
-    const deleteBtn = (e) => {
-        setDeleteIndex(e.target.id);
-    }
-
-    const deleteQue = (e) => {
-        console.log('삭제할 컴포넌트 id', e.target.id);
-        setDelIndex(e.target.id);
-    }
-
-    const ccc = useRef(0);
+    const deleteBtn = (e) => { setDeleteIndex(e.target.id); }
+    const deleteQue = (e) => { setDelIndex(e.target.id); }
 
     const checkCount = (e, addMaxNum) => {
         ccc.current += (e.target.checked)? 1 : -1;
+        // setEssential(ccc.current);
         if(ccc.current>addMaxNum){
             alert('놉!');
             e.target.checked = false;
             ccc.current -= 1;
         }
-    } 
+    }
+
+    useEffect(()=>{
+        if(essential!=0){
+            setSelect([...select]);
+        }
+    },[essential])
+
+
+    useEffect(()=>{
+        console.log("essential !!!!!!!!!!!!!", ccc.current);
+        console.log((ReadOnlyData.surQue_Essential&&ccc.current==0));
+        if(essential!=0){
+            console.log("야!");
+            setSelect([...select]);
+        }
+    },[essential])
     
-    const addText = (number, ReadOnlyData, addMaxNum) => {
+    const addText = (number, ReadOnlyData, addMaxNum, checkBoxEssential, ) => {
         count.current+=1;
-        setTemp([...temp, `SurQue_Ans_${number}_${count.current}`]); // 질문에 대한 보기 이름 덩어리 합치는 중
-        console.log("지금 이 순서대로 합치고 있다. 분명히", `SurQueCheck_${number}_${count.current}`);
+        !UpdateKey && setTemp([...temp, `SurQue_Ans_${number}_${count.current}`]); // 질문에 대한 보기 이름 덩어리 합치는 중
         dispatch(submitAction(`SurQueCheck_${number}_${count.current}`))
         return(
             <Grid key={`SurQue_Ans_${number}_${count.current}`} container spacing={2}>
                 <Grid item xs={11} key={`SurQue_Ans_${number}_${count.current}`}>
-                {ReadOnlyState&&<Checkbox value={ReadOnlyState?ReadOnlyData.surSel_Content:null} name={`SurQueCheck_${number}_${count.current}`} id={`SurQueCheck_${number}_${count.current}`} onClick={(e)=>checkCount(e, addMaxNum)}/>}
-                <TextField
-                    variant="standard"
-                    required
-                    fullWidth
-                    disabled={ReadOnlyState}
-                    name={`SurQue_Ans_${number}_${count.current}`}
-                    id={`SurQue_Ans_${number}_${count.current}`}
-                    label={`선택지${number}_${count.current}`}
-                    value={ReadOnlyState?ReadOnlyData.surSel_Content:null}
-                    >
-                </TextField>
+                    {console.log("hi", (checkBoxEssential&&essential==0))}
+
+                    {(ReadOnlyState&&!UpdateKey)&&
+                        <Checkbox
+                            required={(checkBoxEssential&&essential==0)}
+                            value={ReadOnlyState?ReadOnlyData.surSel_Content:null} 
+                            name={`SurQueCheck_${number}_${count.current}`} 
+                            id={`SurQueCheck_${number}_${count.current}`} 
+                            onClick={(e)=>{ setEssential(1); checkCount(e, addMaxNum);}}
+                        />
+                    }
+                    
+                    <TextField
+                        variant="standard"
+                        required
+                        fullWidth
+                        disabled={ReadOnlyState&&!UpdateKey}
+                        name={`SurQue_Ans_${number}_${count.current}`}
+                        id={`SurQue_Ans_${number}_${count.current}`}
+                        label={`선택지${number}_${count.current}`}
+                        value={ReadOnlyState?ReadOnlyData.surSel_Content:null}
+                        >
+                    </TextField>
                 </Grid>
-                {!ReadOnlyState&&<Grid item xs={1}>
-                <Button 
-                    id={`SurQue_Ans_${number}_${count.current}`}
-                    onClick={(e)=>deleteBtn(e)}
-                    >삭제</Button><br/>
-                </Grid>}
+
+                {(!ReadOnlyState||UpdateKey)&&
+                    <Grid item xs={1}>
+                    <Button 
+                        id={`SurQue_Ans_${number}_${count.current}`}
+                        onClick={(e)=>deleteBtn(e)}
+                        >삭제</Button><br/>
+                    </Grid>
+                }
             </Grid>
         );
     }
@@ -113,6 +137,7 @@ const MultipleChoiceComp = ({number, setCheck, setDelIndex, ReadOnlyState, ReadO
                 deleteQue={deleteQue}
                 ReadOnlyState={ReadOnlyState}
                 ReadOnlyData={ReadOnlyData}
+                UpdateKey={UpdateKey}
             />
         </>
     );
--- front/src/components/survey/comp/SurveySubmitComp.js
+++ front/src/components/survey/comp/SurveySubmitComp.js
@@ -5,63 +5,143 @@ import BeforeSurveyComp from '../comp/BeforeSurveyComp';
 import MainSurveyComp from '../comp/MainSurveyComp';
 import { createTheme } from '@mui/material/styles';
 import { useEffect } from 'react';
-import { getSurvey as getSurveyAPI, postSurvey as postSurveyAPI } from '../../../lib/api/survey';
+import { getSurvey as getSurveyAPI, getTags as getTagsAPI, postSurvey as postSurveyAPI } from '../../../lib/api/survey';
 import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { beforeAction } from '../../../modules/submitReducer';
 import { useHistory } from 'react-router';
 import Swal from 'sweetalert2';
+import MultipleChoiceComp from './MultipleChoiceComp';
+import SubjectiveComp from './SubjectiveComp';
+import LinearMagnificationComp from './LinearMagnificationComp';
 
-const SurveySubmitComp = ({surveykey}) => {
+const SurveySubmitComp = ({surveykey, UpdateKey}) => {
     const sexAge = useSelector(state=>state.submitReducer.beforeData)
     const surAns_Content = useSelector(state=>state.submitReducer.surAns_Content)
     const [activeStep, setActiveStep] = React.useState(0);
-    const steps = ['데이터 수집', '설문지', '제출 완료'];
     const [surveyReqForm, setSurveyReqForm] = useState(null);
+    const [day, setDay] = useState([new Date(), new Date()]); // 수정일때 사용
+    const [tag, setTag] = useState(null); // 수정일때 사용
+    const [tags, setTags] = useState(null); // 수정일때 사용
+    const [question, setQuestion] = useState([]); // 질문 덩어리(객관식, 주관식, 선형배율)
+    const [anchorEl, setAnchorEl] = useState(null);
+    const [check, setCheck] = useState({});
+    const [delIndex, setDelIndex] = useState(null);
+    const [question_ans, setQuestion_Ans] = useState({}); // 질문에 대한 보기 이름에 대한 배열을 보내기 위해
+    const [sex,setSex] = useState();
+    const [age,setAge] = useState();
     const submitCheck = useRef(false);
-    const dispatch = useDispatch()
+    const open = Boolean(anchorEl);
+    const count = useRef(0);
+    const theme = createTheme();
     const history = useHistory();
+    const dispatch = useDispatch()
+    const steps = ['데이터 수집', '설문지', '제출 완료'];
+
+    useEffect(()=>{ // 수정 시, mainSurvey 출력 및 태그 목록 불러오기
+        if(UpdateKey){ 
+            setActiveStep(1); // mainSurveyComp 바로 이동
+            
+            getTagsAPI() // 태그 목록 불러오기
+                .then((res)=>setTags(res.data))
+                .catch(err=>console.log(err));
+        }
+    },[])
 
     useEffect(() => {
         getSurveyAPI(surveykey)
            .then(res =>{ console.log("요청 결과: ",res.data); setSurveyReqForm(res.data); })
-           .catch(err => console.log(err));
+           .catch(err => console.log(err)); // 설문 참여한 사람이라면, 서버쪽에서 알려주어서 튕구는 걸로 함 403
    },[surveykey])
 
-   const [sex,setSex] = useState();
-   const [age,setAge] = useState();
+   useEffect(()=>{
+       if(!UpdateKey&&surveyReqForm){
+           setDay([surveyReqForm.sur_StartDate, surveyReqForm.sur_EndDate]);
+           count.current = surveyReqForm.questionList.length;
+           let newOrderQuestion = surveyReqForm.questionList.map((value, index)=>{
+                switch(value.surQue_QType){
+                    case 0: // 주관식
+                        return <div key={index}><SubjectiveComp ReadOnlyState={true} ReadOnlyData={value} setDelIndex={setDelIndex} number={value.surQue_Order} setCheck={setCheck} UpdateKey={UpdateKey}/></div>;
+                    case 1: // 객관식
+                        return <div key={index}><MultipleChoiceComp ReadOnlyState={true} ReadOnlyData={value} setDelIndex={setDelIndex} number={value.surQue_Order} setCheck={setCheck} UpdateKey={UpdateKey}/></div>;
+                    case 2: // 선형배율
+                        return <div key={index}><LinearMagnificationComp ReadOnlyState={true} ReadOnlyData={value} setDelIndex={setDelIndex} number={value.surQue_Order} setCheck={setCheck} UpdateKey={UpdateKey}/></div>;
+                    default: break;
+                }
+            });
+            setQuestion(newOrderQuestion);
+       }
+   },[surveyReqForm])
 
+    const handleClick = (event) => setAnchorEl(event.currentTarget);
     const getStepContent = (step) => {
         switch (step) {
-        case 0:
-            return <BeforeSurveyComp setAge={setAge} setSex={setSex}/>;
-        case 1:
-            return <MainSurveyComp surveyReqForm={surveyReqForm} /> ;
-        case 2:
-            return  finalStep();
-        default:
-            throw new Error('Unknown step');
+            case 0:
+                return <BeforeSurveyComp setAge={setAge} setSex={setSex}/>;
+            case 1:
+                return <MainSurveyComp 
+                            surveyReqForm={surveyReqForm} 
+                            UpdateKey={UpdateKey} 
+                            day={day} 
+                            setDay={setDay} 
+                            tag={tag} 
+                            setTag={setTag} 
+                            tags={tags} 
+                            handleClick={handleClick}
+                            anchorEl={anchorEl}
+                            open={open}
+                            handleClose={handleClose}
+                            question={question}
+                            ReadOnlyState={true}
+                        /> ;
+            case 2:
+                return  <React.Fragment>
+                            <Typography variant="h5" gutterBottom>
+                                설문 감사합니다 :)
+                            </Typography>
+                        </React.Fragment>;
+            default:
+                throw new Error('Unknown step');
         }
     }
-
-    const finalStep = () => {
-    return(
-    <React.Fragment>
-        <Typography variant="h5" gutterBottom>
-            설문 감사합니다 :)
-        </Typography>
-    </React.Fragment>);
-    }
-    const theme = createTheme();
     
-    const handleNext = () => {
-      setActiveStep(activeStep + 1);
+    useEffect(()=>{
+        setQuestion_Ans({...question_ans, ...check}); // 객관식, 주관식, 선형 배율의 보기들을 합치는 곳
+    },[check]);
+    
+    useEffect(()=>{
+        // 객,주,선 삭제
+        const newQuestionList = question.filter((value)=> value.key!==delIndex );
+        setQuestion(newQuestionList);
+
+        // 해당 객관식 보기 삭제
+        for (const key in question_ans) {
+            if(key == delIndex)
+                delete question_ans[key];
+        }
+    },[delIndex]);
+
+
+    const handleClose = (e) => {
+        setAnchorEl(null); // 메뉴 닫기
+        switch(e.target.id){
+            case '객관식':
+                setQuestion([...question, <div key={count.current}><MultipleChoiceComp ReadOnlyState={false} ReadOnlyData={null} setDelIndex={setDelIndex} number={count.current} setCheck={setCheck} UpdateKey={false}/></div>  ]);
+                break;
+            case '주관식':
+                setQuestion([...question, <div key={count.current}><SubjectiveComp ReadOnlyState={false} ReadOnlyData={null} setDelIndex={setDelIndex} number={count.current} setCheck={setCheck} UpdateKey={false}/></div>  ]);
+                break;
+            case '선형배율':
+                setQuestion([...question, <div key={count.current}><LinearMagnificationComp ReadOnlyState={false} ReadOnlyData={null} setDelIndex={setDelIndex} number={count.current} setCheck={setCheck} UpdateKey={false}/></div>  ]);
+                break;
+            default : break;
+        }
+        count.current+=1;
     };
 
     const lastSubmit = (e) => {
         e.preventDefault();
         if (submitCheck.current === false){
-            console.log("왔나");
             submitCheck.current = true
         }else{
             const data = new FormData(e.currentTarget);
@@ -71,7 +151,6 @@ const SurveySubmitComp = ({surveykey}) => {
             let OrderNumber = 0;
             let tempString = "";
             
-            console.log("surAns_Content", surAns_Content);
             surAns_Content.map((value, index) =>{
                 let splitValue=value.split('_');
                 temp = data.get(value);
@@ -123,20 +202,16 @@ const SurveySubmitComp = ({surveykey}) => {
             const answerList = tempArray.map((v)=>{
                 return {'surAns_Content':v}
             })
-            console.log("tempArray:",tempArray);
-            console.log("잘 있나", sexAge.age, sexAge.sex);
             postSurveyAPI(surveykey,{"age":sexAge.age, "gender":sexAge.sex, "answerList":answerList})
                 .then(res=>console.log("제출 성공..?",res))
                 .catch(res=>console.log("제출 실패..?",res))
             
             submitCheck.current = false;
         }
-        handleNext();
+        setActiveStep(activeStep + 1); // 다음 페이지로 이동
     }
 
-    const nextPage = () => {
-        dispatch(beforeAction({age:age,sex:sex}))
-    }
+    const nextPage = () => dispatch(beforeAction({age:age,sex:sex}));
 
     const wayBackHome = () =>{
         Swal.fire('Way Back Home ~');
@@ -148,13 +223,13 @@ const SurveySubmitComp = ({surveykey}) => {
             <SurveySubmit 
                 steps={steps}
                 getStepContent={getStepContent}
-                finalStep={finalStep}
                 theme={theme}
                 activeStep={activeStep}
                 setActiveStep={setActiveStep}
                 lastSubmit={lastSubmit}
                 nextPage={nextPage}
                 wayBackHome={wayBackHome}
+                UpdateKey={UpdateKey}
             />   
         </>
     );
