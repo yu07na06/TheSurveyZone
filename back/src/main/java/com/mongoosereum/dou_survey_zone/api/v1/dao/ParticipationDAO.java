9e83e9ddc1ddb8c132ddda5e1bc1fbc59e4c02bb
Domo9610 <54881558+Domo9610@users.noreply.github.com>
2021-11-30 16:29:46 +0900

    Feat_Back/DAOImpl

--- back/src/main/java/com/mongoosereum/dou_survey_zone/api/v1/dao/ParticipationDAO.java
+++ back/src/main/java/com/mongoosereum/dou_survey_zone/api/v1/dao/ParticipationDAO.java
@@ -1,41 +1,15 @@
 package com.mongoosereum.dou_survey_zone.api.v1.dao;
 
 import com.mongoosereum.dou_survey_zone.api.v1.domain.participation.Participation;
-import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.session.SqlSession;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import java.util.HashMap;
 import java.util.Map;
 
-@Service
-public class ParticipationDAO{
-    @Autowired
-    private SqlSession sqlSession;
+public interface ParticipationDAO {
 
-    public Long ACCTotal() {
-        return sqlSession.selectOne("countTotal");
-    }
-    public Map<String, Long> ACCAge() {
-        return sqlSession.selectOne("countAge");
-    }
-    public Map<String,Long> ACCGender() {
-        return sqlSession.selectOne("countGender");
-    }
-    public int findByIP(String _id, String ip){
-        Map<String, String> map = new HashMap<>();
-        System.out.println(ip+ " "+_id);
-        map.put("Part_IP",ip);
-        map.put("_id",_id);
-        System.out.println(ip+ " " + _id);
-        return sqlSession.selectOne("findByIP", map);
-    }
-    public int insertParticipation(Participation participation){
-        return sqlSession.insert("insertParticipation", participation);
-    }
+        Long ACCTotal();
+        Map<String, Long> ACCAge();
+        Map<String,Long> ACCGender();
+        int findByIP(String _id, String ip);
+        int insertParticipation(Participation participation);
+        Participation selectParticipation(Participation participation);
 
-    public Participation selectParticipation(Participation participation){
-        return sqlSession.selectOne("selectParticipation", participation);
-    }
 }
--- back/src/main/java/com/mongoosereum/dou_survey_zone/api/v1/dao/TagDAO.java
+++ back/src/main/java/com/mongoosereum/dou_survey_zone/api/v1/dao/TagDAO.java
@@ -2,31 +2,13 @@ package com.mongoosereum.dou_survey_zone.api.v1.dao;
 
 import com.mongoosereum.dou_survey_zone.api.v1.domain.tag.SurveyTag;
 import com.mongoosereum.dou_survey_zone.api.v1.domain.tag.Tag;
-import org.apache.ibatis.session.SqlSession;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service
-@Repository
-public class TagDAO {
-    @Autowired
-    private SqlSession sqlSession;
+public interface TagDAO {
 
-    /* 존재하는 태그들 */
-    public List<Tag> selectTagList(){
-        return sqlSession.selectList("selectTagList");
-    }
+    List<Tag> selectTagList();
+    List<Tag> findById(String _id);
+    int insertTag(SurveyTag surveyTag);
 
-    /* survey에 사용된 태그들  */
-    public List<Tag> findById(String _id){
-        return sqlSession.selectList("selectExistTagList",_id);
-    }
-
-    /*태그 생성*/
-    public int insertTag(SurveyTag surveyTag){
-        return sqlSession.insert("insertSurveyTag",surveyTag);
-    }
 }
