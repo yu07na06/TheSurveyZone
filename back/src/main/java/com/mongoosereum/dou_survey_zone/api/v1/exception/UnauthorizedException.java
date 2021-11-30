bc737daf300e5d671bd2374232221f608bbeaff6
hwani <ojh2134@gmail.com>
2021-11-30 17:17:41 +0900

    Refactor_Back/Exceptions

--- back/src/main/java/com/mongoosereum/dou_survey_zone/api/v1/exception/UnauthorizedException.java
+++ back/src/main/java/com/mongoosereum/dou_survey_zone/api/v1/exception/UnauthorizedException.java
@@ -1,7 +1,7 @@
 package com.mongoosereum.dou_survey_zone.api.v1.exception;
 
 public class AuthenticationException extends RuntimeException{
-    private ErrorCode errorCode;
+    public ErrorCode errorCode;
     public AuthenticationException(ErrorCode errorCode){
         super(errorCode.getMessage());
         this.errorCode = errorCode;
