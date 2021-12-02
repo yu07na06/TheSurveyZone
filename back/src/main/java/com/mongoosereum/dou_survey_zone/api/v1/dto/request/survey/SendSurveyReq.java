83f9435108ae39c09ccd0e5bb3e3c863b23a5a03
hwani <ojh2134@gmail.com>
2021-12-02 21:59:38 +0900

    Fix_Back/InsertSurvey_NoImage

--- back/src/main/java/com/mongoosereum/dou_survey_zone/api/v1/dto/request/survey/SendSurveyReq.java
+++ back/src/main/java/com/mongoosereum/dou_survey_zone/api/v1/dto/request/survey/SendSurveyReq.java
@@ -2,18 +2,23 @@ package com.mongoosereum.dou_survey_zone.api.v1.dto.request.survey;
 
 import com.mongoosereum.dou_survey_zone.api.v1.domain.user.User;
 import com.mongoosereum.dou_survey_zone.api.v1.dto.response.survey.SelectSurveyRes;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
+@ApiModel("이메일 전송 DTO")
 public class SendSurveyReq {
 
     private User from;
 
+    @ApiModelProperty("설문 ID")
     private String _id;
 
     private SelectSurveyRes survey;
 
+    @ApiModelProperty("이메일 List")
     private List<String> emailList;
 }
