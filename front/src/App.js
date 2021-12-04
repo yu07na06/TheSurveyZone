af8e3ee2e61a49ea7d576f0d1ba135a1cfbfbd73
Domo9610 <54881558+Domo9610@users.noreply.github.com>
2021-12-04 17:55:19 +0900

    Feat_Front/UI[Main Header Modify UI]

--- front/src/App.js
+++ front/src/App.js
@@ -64,10 +64,12 @@ import RegisterPage from './pages/RegisterPage';
 import ResultPage from './pages/ResultPage';
 import SurveySubmitPage from './pages/SurveySubmitPage';
 import UpdatePage from './pages/UpdatePage';
+import styled from 'styled-components'
 
 const App = () => {
   return (
     <>
+      <Container>
       <Route path="/" component={MainPage} exact/>
       <Route path="/LoginPage" component={LoginPage} />
       <Route path="/RegisterPage" component={RegisterPage} />
@@ -85,8 +87,18 @@ const App = () => {
       <Route path="/UpdatePage/:surveykey" component={UpdatePage} /> 
       {/* surveykey : 설문지의 고유번호(PK)를 의미 */}
       <Route path="/ResultPage/:surveykey" component={ResultPage} />
+      </Container>
     </>
   );
 }
+
+const Container = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  background: #FCFCFC;
+  background-size: cover;
+`;
 
 export default App;
